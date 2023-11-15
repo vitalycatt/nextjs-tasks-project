b50cda51cba4ea225be8aa70ae45469cf918165c
vitaliycatt <ksowy@mail.ru>
2023-11-15 15:01:11 +0300

    feat: create layout & add font & add eslint settings

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,24 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import classnames from "classnames";
+import { Kanit } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const kanit = Kanit({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={classnames("mx-20", [inter.className])}>
-        <header className="flex items-center justify-between pt-6 mb-6">
-          <div>U can put here our logo</div>
+      <body className={classnames("", [kanit.className])}>
+        <header className="flex items-center justify-between pt-6 mb-6 mx-20">
+          <h1 className="font-extralight italic text-2xl">
+            U can put your logo here
+          </h1>
 
-          <div className="flex items-center justify-end">
+          <div className="flex items-center justify-end font-medium italic text-lg">
             <Link href="/" className="mr-4">
               Home page
             </Link>
@@ -25,7 +31,9 @@ const RootLayout = ({ children }) => {
           </div>
         </header>
 
-        <main>{children}</main>
+        <hr className="h-0 mb-5 border-b-1 border-black border-solid" />
+
+        <main className="mx-20">{children}</main>
       </body>
     </html>
   );
